c8d5cd5e23524ebbd1a5d54e3dc8d82c8a6de1a0
unknown <joygoswaminiloy2023@gmail.com>
2026-05-02 11:42:37 +0600

    url changed

--- src/app/db/db.js
+++ src/app/db/db.js
@@ -1,5 +1,5 @@
 export const product=async()=>{
-    const res=await fetch("http://localhost:3000/data.json");
+    const res=await fetch("https://sun-cart-self.vercel.app/data.json");
     const data= await res.json()
     return data;
 }
